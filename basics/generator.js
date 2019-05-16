07418357b81486e8362c12d5f930051caaea2f41
Om Prakash C <comprakash@gmail.com>
2019-05-16 14:12:29 +0530

    Generators

---
+++ basics/generator.js
@@ -0,0 +1,16 @@
+function * generatorFunction() { 
+    console.log('This will be executed first.');
+
+    yield 'JS - Powerful ';   
+
+    console.log('printed after the pause');
+
+    yield 'Language.';
+}
+
+
+const generatorObject = generatorFunction(); 
+
+console.log(generatorObject.next().value); 
+console.log(generatorObject.next().value); 
+console.log(generatorObject.next().value); 
